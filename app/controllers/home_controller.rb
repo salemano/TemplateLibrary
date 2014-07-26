95c4508d15d411b9c3ab75ba4025212c2c92f866
Sergey <user@Admin.(none)>
2014-07-26 20:43:36 +0300

    Added Basic Front-end

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def Index
-    @Temp = 'This is home page of my page!'
+    @Header = 'Form title'
+    @FormText = 'Put any text here'
+    @FooterText = 'Footer text'
   end
 end
