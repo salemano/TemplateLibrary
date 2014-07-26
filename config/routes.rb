b02f7ee22f8a90a4d3f9d8051c35703320b874c7
user <user@Admin.(none)>
2014-07-26 16:01:57 +0300

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TemplateLibrary::Application.routes.draw do
   resources :users
+  resources :templates
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
